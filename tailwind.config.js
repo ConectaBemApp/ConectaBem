7c18956d345e7324b0efefd30b251aba8cfa13aa
HUDEVBR <forhudson@hotmail.com>
2025-02-10 13:39:02 -0300

    Criada tela do paciente 1

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,html}"], // Adicione os caminhos corretos
+  theme: {
+    extend: {
+      colors: {
+        buttonColor: '#D7FF7B',
+      },
+    },
+  },
+  plugins: [],
+};
